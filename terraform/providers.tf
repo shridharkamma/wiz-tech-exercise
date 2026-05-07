e5a78db6046cf4affd7041f4fc8815e4a98c5b07
shridharkamma <skshridar@gmail.com>
2026-05-08 00:13:57 +0100

    Configure Terraform remote state backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "s3" {
+    bucket         = "wiztestv1-tfstate-985539797171"
+    key            = "wiz-tech-exercise/terraform.tfstate"
+    region         = "us-east-1"
+    use_lockfile = true
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
