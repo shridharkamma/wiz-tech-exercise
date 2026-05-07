953bab1c26a4cf5589f914d453c7e24aa06ded63
shridharkamma <skshridar@gmail.com>
2026-05-07 13:24:53 +0100

    Create VPC networking for EKS

---
+++ terraform/networking.tf
@@ -0,0 +1,50 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name = "${var.resource_prefix}VPC"
+
+  cidr = "10.0.0.0/16"
+
+  azs = [
+    "${var.region}a",
+    "${var.region}b"
+  ]
+
+  public_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_subnets = [
+    "10.0.11.0/24",
+    "10.0.12.0/24"
+  ]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    "Type" = "${var.resource_prefix}publicsubnet"
+
+    "kubernetes.io/role/elb" = "1"
+
+    "kubernetes.io/cluster/${var.resource_prefix}EKS" = "shared"
+  }
+
+  private_subnet_tags = {
+    "Type" = "${var.resource_prefix}privatesubnet"
+
+    "kubernetes.io/role/internal-elb" = "1"
+
+    "kubernetes.io/cluster/${var.resource_prefix}EKS" = "shared"
+  }
+
+  tags = {
+    Environment = var.resource_prefix
+    Project     = "WizExercise"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_prefix" {
+  description = "Prefix for all resources"
+  type        = string
+  default     = "WizTestv1"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
