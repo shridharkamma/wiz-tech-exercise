fbaf9eb8fd410b7308a36b4cd1c2c2444d67d772
shridharkamma <skshridar@gmail.com>
2026-05-08 14:23:43 +0100

    Format Terraform provider config

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket         = "wiztestv1-tfstate-985539797171"
-    key            = "wiz-tech-exercise/terraform.tfstate"
-    region         = "us-east-1"
+    bucket       = "wiztestv1-tfstate-985539797171"
+    key          = "wiz-tech-exercise/terraform.tfstate"
+    region       = "us-east-1"
     use_lockfile = true
-    encrypt        = true
+    encrypt      = true
   }
 
   required_providers {
